a3cb1a06bd5e91f054fcd333f031d396284bd9f4
Francesco Falone <252209716+frafalone-art@users.noreply.github.com>
2026-09-07 13:14:38 +0200

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,3 +1,6 @@
+[BITS 16]
+[ORG 0x7C00]
+
 start:
     mov si, greeting
     call print_string
@@ -18,3 +21,26 @@ start:
     jc disk_error
 
     jmp 0x1000
+    
+print_string:
+    lodsb
+    cmp al, 0
+    je print_done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_string
+print_done:
+    ret
+
+disk_error:
+    mov si, err_msg
+    call print_string
+    cli
+    hlt
+    jmp $
+
+greeting db 'Kiwi OS booting...', 0
+err_msg db 'Disk error!', 0
+
+times 510-($-$$) db 0
+dw 0xAA55
